26200eb7b5dcfb92a2041b820272b647864d8bd9
gtMarat <marat19955@gmail.com>
2023-03-12 15:36:41 +0300

    hw completed

---
+++ src/Transport.java
@@ -0,0 +1,20 @@
+public abstract class Transport  {
+    private String modelName;
+    private int wheelsCount;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public Transport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
